f4cded709e5b7ffd321730fc9605aa026b51c941
Ashwini <pardeshia1998@gmail.com>
2022-01-26 11:32:46 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <Hello />
+      <Hello />
+      <World />
+    </>
   );
 }
 
-export default App;
+//JSX => X = >XML
+//JSX :: REACT COMPONENT
+function Hello() {
+  return <h1>Hello!!</h1>;
+}
+
+function World() {
+  return <h1>Wolrd...</h1>;
+}
