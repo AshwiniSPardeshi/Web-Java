4657bd85c7c91228bce331634788685435983f94
Ashwini <pardeshia1998@gmail.com>
2022-01-26 11:56:21 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 export default function App() {
+  let p1 = "Ashwini";
+  console.log(p1);
   return (
     <>
-      <h1>Hello World</h1>
-      <Hello />
-      <Hello />
-      <World />
+      <h1>Hello World {p1} </h1>
+      console.log(p1);
+      <Hello name="Teju" />
+      <Hello id="1" name="Pranabh" />
     </>
   );
 }
 
 //JSX => X = >XML
 //JSX :: REACT COMPONENT
-function Hello() {
-  return <h1>Hello!!</h1>;
-}
+/*function Hello(p) {
+  return <h1>Hello!! {p.name} </h1>;  // as variable parametre passed we have to use p.name
+}*/
 
-function World() {
-  return <h1>Wolrd...</h1>;
+function Hello(name, id) {
+  return <h1>{name} </h1>; ///
 }
